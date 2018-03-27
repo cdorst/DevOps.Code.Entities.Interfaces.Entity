1d2bc76ec9ffd44bf512601dba51a30d91427485
Christopher Dorst <chris@dorst.email>
2018-03-26 20:02:01 -0500

    Add IEntity.cs

---
+++ DevOps.Code.Entities.Interfaces.Entity/IEntity.cs
@@ -0,0 +1,15 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
+namespace DevOps.Code.Entities.Interfaces.Entity
+{
+    /// <summary>Common interface for code-generated entity types</summary>
+    public interface IEntity<TKey>
+    {
+        /// <summary>Returns the unqiue identifier of the entity type</summary>
+        int GetEntityType();
+
+        /// <summary>Returns the unqiue identifier of this entity instance</summary>
+        TKey GetKey();
+    }
+}
